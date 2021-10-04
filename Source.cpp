b0f08a32f958cef397f90080ee5b24a0e1c52c0d
Daulyatshina-Aigul <91548084+Daulyatshina-Aigul@users.noreply.github.com>
2021-10-04 21:10:28 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,69 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main1() {
+int main() {
 	setlocale(LC_ALL, "Russian");
-	int a;
-	cin >> a;
-	if (a > 99 && a < 1000)
-		cout << "верно";
-	else
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (0 < a < 32 && 0 < b < 13 && c > 0) {
+		if (c % 4 == 0) {
+			switch (b) {
+			case 1: cout << 366 - a;
+				break;
+			case 2: cout << 366 - a - 31;
+				break;
+			case 3: cout << 366 - a - 31 - 29;
+				break;
+			case 4: cout << 366 - a - 31 * 2 - 29;
+				break;
+			case 5: cout << 366 - a - 31 * 2 - 29 - 30;
+				break;
+			case 6: cout << 366 - a - 31 * 3 - 29 - 30;
+				break;
+			case 7: cout << 366 - a - 31 * 3 - 29 - 30 * 2;
+				break;
+			case 8: cout << 366 - a - 31 * 4 - 29 - 30 * 2;
+				break;
+			case 9: cout << 366 - a - 31 * 5 - 29 - 30 * 2;
+				break;
+			case 10:cout << 366 - a - 31 * 5 - 29 - 30 * 3;
+				break;
+			case 11:cout << 366 - a - 31 * 6 - 29 - 30 * 3;
+				break;
+			case 12:cout << 366 - a - 31 * 6 - 29 - 30 * 4;
+				break;
+			}
+		}
+		else {
+			switch (b) {
+			case 1: cout << 365 - a;
+				break;
+			case 2: cout << 365 - a - 31;
+				break;
+			case 3: cout << 365 - a - 31 - 28;
+				break;
+			case 4: cout << 365 - a - 31 * 2 - 28;
+				break;
+			case 5: cout << 365 - a - 31 * 2 - 28 - 30;
+				break;
+			case 6: cout << 365 - a - 31 * 3 - 28 - 30;
+				break;
+			case 7: cout << 365 - a - 31 * 3 - 28 - 30 * 2;
+				break;
+			case 8: cout << 365 - a - 31 * 4 - 28 - 30 * 2;
+				break;
+			case 9: cout << 365 - a - 31 * 5 - 28 - 30 * 2;
+				break;
+			case 10:cout << 365 - a - 31 * 5 - 28 - 30 * 3;
+				break;
+			case 11:cout << 365 - a - 31 * 6 - 28 - 30 * 3;
+				break;
+			case 12:cout << 365 - a - 31 * 6 - 28 - 30 * 4;
+				break;
+			}
+		}
+	}
+	else {
 		cout << "ошибка";
+	}
 	return 0;
 }
